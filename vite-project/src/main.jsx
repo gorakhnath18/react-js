a0583ba392f2e235e5f93f3796bbed8e7a06342b
git config --global user.name <gorakhnathx34@gmail.com>
2025-04-04 15:03:09 +0530

    innitial commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom app !   df</h1>
+        </div>
+    )
+}
+
+const ReactElement ={
+    type : 'a',
+    props:{
+        href:'https://google.com',
+        target :'_blank'
+    },
+    chlidren: 'Click me to visit google'
+}
+const anotherElement =(
+    <a href="https://google.com"target='_blank'>visit google</a>
+)
+
 createRoot(document.getElementById('root')).render(
-     <App />
- )
+ anotherElement
+)
